bf42e405cad4592a4a00c2cb7bcea6a327d7ed39
Saubhagya Singh <76896543+SaubhagyaSingh@users.noreply.github.com>
2023-02-28 00:38:15 +0530

    day2 update

---
+++ day1/main.py
@@ -0,0 +1,5 @@
+# band name generator
+print("Welcome to the band name generator")
+city = input("Enter the city you grew up in\n")
+cool = input("Enter a cool thing\n")
+print("Your band name is " + city + " " + cool)
