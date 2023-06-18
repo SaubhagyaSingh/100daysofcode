e78e1d823daa7684890c4044be6b06e1917e3ade
Saubhagya Singh <76896543+SaubhagyaSingh@users.noreply.github.com>
2023-06-19 02:55:07 +0530

    basic turtle code to form a square

---
+++ day18/square.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+saubhi = Turtle()
+saubhi.shape("arrow")
+saubhi.color("red")
+saubhi.forward(100)
+saubhi.right(90)
+saubhi.forward(100)
+saubhi.right(90)
+saubhi.forward(100)
+saubhi.right(90)
+saubhi.forward(100)
+saubhi.right(360)
+
+
+screen = Screen()
+screen = exitonclick()
